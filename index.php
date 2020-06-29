1b36a8cfe13123963d8b4af3350fce352b799d67
martamazurkiewicz <marta@mazurkiewi.cz>
2020-06-29 11:03:18 +0200

    Added navbar to signing pages

--- index.php
+++ index.php
@@ -3,9 +3,17 @@ require_once('navbar.php');
 ?>
 <div style="text-align:center;">
     <img src="memorygame.jpg">
-    <p> Memory game with self drown pictures. Written mostly in php </p>
-    <form action="game.php">
-        <button type="submit" class="btn btn-warning btn-lg">Play</button>
+    <h4> Memory game with self drown pictures. Written mostly in php. </h4>
+    <button type="button" class="btn btn-warning btn-lg" style="width: 160px;" onClick="document.location.href='game.php'">Play</button>
+    <?php
+    if (!isset($_SESSION['username'])) {
+    ?>
+        <p> or </p>
+        <button type="button" class="btn btn-primary" style="width: 100px;" onClick="document.location.href='login.php'">Sign In</button>
+        <button type="button" class="btn btn-primary" style="width: 100px;" onClick="document.location.href='register.php'">Sign Up</button>
+    <?php
+    }
+    ?>
     </form>
 </div>
 </body>
--- register.php
+++ register.php
@@ -9,10 +9,11 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sign up!</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Memory Game</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <style>
         .login-form {
             width: 340px;
@@ -69,10 +70,25 @@ if (session_status() == PHP_SESSION_NONE) {
 </head>
 
 <body>
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Memory Game</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/game.php">New game</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/score.php">Scoreboard</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div class="login-form">
         <form method="POST" action="registerHandler.php">
             <?php
-            //var_dump(isset($_SESSION['message']));
             if (isset($_SESSION['message'])) {
             ?>
                 <div class='alert'>
